117c00b70df8a62f111d73e6009501fdfb79c338
Vipul Kumawat <>
2024-06-29 14:13:33 +0530

    Initial Create

---
+++ Movie Database App/Movie Database App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie Database App
+//
+//  Created by Vipul Kumawat on 29/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
